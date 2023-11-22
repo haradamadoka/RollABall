758199403a33cf18a612ea01a4cb024b4ae2c1f4
原田和香 <vga29-ac220059@sankogakuen.jp>
2023-11-22 14:57:14 +0900

    制限時間と邪魔ブロック追加

---
+++ Assets/Scripts/RollABall/InGame/BallInput.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 新しいInput Systemを使用して"W"キーの入力を検知
+        if (Keyboard.current.wKey.wasPressedThisFrame)
+        {
+            Debug.Log("Wキーが押されました!");
+        }
+        // Wキーが押され続けている間
+        if (Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("Wキーが押され続けてます!");
+        }
+        // Wキーが離された瞬間だけ評価する
+        if (Keyboard.current.wKey.wasReleasedThisFrame)
+        {
+            Debug.Log("Wキーが離されました!");
+        }
+    }
+}
---
+++ Assets/Scripts/RollABall/InGame/PendulumBlock.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PendulumBlock : MonoBehaviour
+{
+    public float Amplitude = 5f; //êUïù
+    public float Frequency = 1f; //ïpìx
+    private Vector3 startPos = Vector3.zero;
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+    void Update()
+    {
+        transform.position = startPos + Vector3.forward * Amplitude * Mathf.Sin(Time.time * Frequency);
+    }
+}
