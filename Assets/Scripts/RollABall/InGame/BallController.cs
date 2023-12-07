625346e995d70e35d13ad6fe059522cca9b23df3
原田和香 <vga29-ac220059@sankogakuen.jp>
2023-12-07 15:47:33 +0900

    URP,Piazzaアセット

--- Assets/Scripts/RollABall/InGame/BallController.cs
+++ Assets/Scripts/RollABall/InGame/BallController.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     private Rigidbody ballRigidBody;
+    private BallData ballData;
+    private InGameTimer InGameTimer;
+
     private void Start()
     {
         ballRigidBody = this.GetComponent<Rigidbody>();
         var ballData = new BallData(5);
         Debug.Log(ballData.GetHitPoint);//5
     }
+
+    private void Update()
+    {
+        if (InGameTimer.GetTimerTime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     public void BallMove(Vector3 direction)
     {
         ballRigidBody.AddForce(direction);
--- Assets/Scripts/RollABall/InGame/Coin.cs
+++ Assets/Scripts/RollABall/InGame/Coin.cs
@@ -9,10 +9,10 @@ public class Coin : MonoBehaviour
     private float bonusTime = 2f;
     private float disappearTime = 5f;
 
-    private int scoreValue = 10;
-    private int bonusScoreValue = 20;
+    private int scoreValue = 10;//通常スコア値
+    private int bonusScoreValue = 20;//ボーナススコア値
 
-    private int bigBonusScoreValue = 100;
+    private int bigBonusScoreValue = 100;//ビッグボーナススコア値
 
     private enum bonusChance
     {
@@ -24,16 +24,22 @@ public class Coin : MonoBehaviour
 
     private bonusChance bonusChances;
 
+    public Material CoinMaterial;
+
     void Start()
     {
         StartCoroutine(BonusTimeCoroutine());
     }
     IEnumerator BonusTimeCoroutine()
     {
-        // ここまではボーナスタイムの2秒待つ
-        yield return new WaitForSeconds(bonusTime);
-        
-        // 消える時間は5秒なので、ボーナスタイムを2秒消費した3秒待つ
+        bonusChances = bonusChance.BigBonus;
+        CoinMaterial.color = Color.yellow;
+        yield return new WaitForSeconds(bigBonusTime);
+        bonusChances = bonusChance.SmallBonus;
+        CoinMaterial.color = Color.cyan;
+        yield return new WaitForSeconds(bonusTime - bigBonusTime);
+        bonusChances = bonusChance.NoBonus;
+        CoinMaterial.color = Color.red;
         yield return new WaitForSeconds(disappearTime - bonusTime);
         Destroy(this.gameObject);
     }
@@ -42,15 +48,19 @@ public class Coin : MonoBehaviour
     {
         if (other.gameObject.name == "Sphere")
         {
-            if (isBonus)
+            switch (bonusChances)
             {
-                ScoreManager.ScoreCount += bonusScoreValue;
+                case bonusChance.BigBonus:
+                    ScoreManager.ScoreCount += bigBonusScoreValue;
+                    break;
+                case bonusChance.SmallBonus:
+                    ScoreManager.ScoreCount += bonusScoreValue;
+                    break;
+                case bonusChance.NoBonus:
+                    ScoreManager.ScoreCount += scoreValue;
+                    break;
             }
-            else
-            {
-                ScoreManager.ScoreCount += scoreValue;
-            }
-
+            //コイン消去
             Destroy(gameObject);
         }
     }
