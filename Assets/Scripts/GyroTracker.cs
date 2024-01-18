2e26f2c6e5879b1fe9b524e1bb7b837684bc7f93
原田和香 <vga29-ac220059@sankogakuen.jp>
2024-01-18 14:19:33 +0900

    Gyro

---
+++ Assets/Scripts/GyroTracker.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroTracker : MonoBehaviour
+{
+    // ジャイロスコープクラスの参照をキャッシュ
+    public Gyroscope gyro;
+
+    // イベントをトリガーするメソッド
+    public void OnEnable()
+    {
+        // デバイスがジャイロをサポートしているか確認
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+        }
+    }
+
+    protected void OnGUI()
+    {
+        if (gyro == null)
+        {
+            return;
+        }
+
+        GUILayout.Label("input.gyro.attitude: " + gyro.attitude);
+        GUILayout.Label("input.gyro.userAcceleration: " +
+        gyro.userAcceleration);
+    }
+
+}
