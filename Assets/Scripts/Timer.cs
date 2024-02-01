6256f02f09bcb173b3af081cae81de6b9845cc2f
原田和香 <vga29-ac220059@sankogakuen.jp>
2024-02-01 15:50:47 +0900

    Timerまで

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+	[Tooltip("タイマーが開始してからの時間"), SerializeField]
+	private float elapsedTime;
+
+	[Tooltip("タイマーの持続時間"), SerializeField]
+	private float duration;
+
+	[Tooltip("パーセンテージで出力する値"), SerializeField]
+	private bool percentage;
+
+	[System.Serializable]
+	public class FloatUnityEvent : UnityEvent<float> { }
+
+	[Tooltip("パーセンテージで処理される値"), SerializeField]
+	public FloatUnityEvent elapsedTimeUpdated;
+
+	[Tooltip("時間が切れると呼び出される"), SerializeField]
+	public UnityEvent completed;
+
+
+	public float GetProgress(bool asPercentage = true)
+	{
+		// 保存された値
+		float result;
+
+		// パーセンテージ、または経過時間
+		if (asPercentage)
+		{
+			result = elapsedTime / duration;
+		}
+		else
+		{
+			result = elapsedTime;
+		}
+
+		// 最大値内に収める 
+		return Mathf.Clamp(result, 0, asPercentage ? 1 : duration);
+	}
+
+
+	// タイマーを更新
+	public IEnumerator PerformTimeCheck()
+	{
+
+		// 条件が真である限り実行
+		while (elapsedTime < duration)
+		{
+			// 各ループがフレームの終わりまで待つよう強制
+			yield return new WaitForEndOfFrame();
+
+			// 経過時間を更新 
+			elapsedTime += Time.deltaTime;
+
+			// リスナーにelapsedTimeが更新されたことを通知 
+			elapsedTimeUpdated?.Invoke(GetProgress(percentage));
+		}
+
+		// リスナーに時間終了を通知
+		completed?.Invoke();
+	}
+
+	// タイマーを初期値にリセット
+	public void ResetTime()
+	{
+		elapsedTime = 0;
+		elapsedTimeUpdated.Invoke(GetProgress(percentage));
+	}
+
+	public void StartTimer()
+	{
+		StopAllCoroutines();
+		StartCoroutine(PerformTimeCheck());
+	}
+
+	public void RestartTimer()
+	{
+		// 時間を0にリセットし、サブスクライバに通知
+		ResetTime();
+
+		// タイマーを開始
+		StartTimer();
+	}
+
+}
---
+++ Assets/Scripts/VRRaycaster.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VRRaycaster : MonoBehaviour
+{
+	[Tooltip("The layers considered as valid targets")]
+	public LayerMask layerMasks;
+
+	[Tooltip("maximum detection distance")]
+	public float distance;
+
+	[Tooltip("Game object currently being hit")]
+	private GameObject hitGameObject;
+
+	[System.Serializable]
+	public class GameObjectUnityEvent :
+	UnityEvent<GameObject>
+	{ }
+
+	[Tooltip("Event when hovering a target passed as argument")]
+	public GameObjectUnityEvent hitTarget;
+
+	[System.Serializable]
+	public class Vector3UnityEvent : UnityEvent<Vector3> { }
+
+	[Tooltip("Information relative to a hit 3D properties")]
+	public Vector3UnityEvent hitPosition, hitAngle;
+
+	[Tooltip("Event sent when no longer over a target")]
+	public UnityEvent stopHittingTarget;
+
+
+    public void Raycast()
+	{
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hitInfo;
+		bool hasHit =
+		Physics.Raycast(ray, out hitInfo, distance, layerMasks);
+
+		// 当たった場合当たった対象に関連するイベントを更新
+		if (hasHit)
+		{
+			if (hitGameObject != hitInfo.transform.gameObject)
+			{
+				hitGameObject = hitInfo.transform.gameObject;
+				hitTarget?.Invoke(hitGameObject);
+				Debug.Log(hitInfo.transform.gameObject.name);
+			}
+			hitAngle?.Invoke(hitInfo.normal);
+			hitPosition?.Invoke(hitInfo.point);
+		}
+		else if (hitGameObject)
+		{
+			stopHittingTarget?.Invoke();
+		}
+	}
+
+    private void Update()
+    {
+		Raycast();
+    }
+
+
+
+}
